3b2f259bcbdad92d7161a632d24f01311c86d23d
Tony Gaddy <ragaddy@richmondcc.edu>
2018-06-13 13:35:37 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -174,6 +174,17 @@ app.post('/users/login', (req, res) => {
 
 });
 
+// DELETE
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });

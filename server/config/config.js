ffa6ee1ca434b1a4034b2786ba3bb1e085bbb41c
Tony Gaddy <ragaddy@richmondcc.edu>
2018-06-14 08:28:24 -0400

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MOGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MOGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else {
-  process.env.MOGODB_URI = 'mongodb://todouser:iNinja#1@ds113775.mlab.com:13775/todoapp';
+
+if (env === 'development' || env === 'test') {
+  let config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+
 }
